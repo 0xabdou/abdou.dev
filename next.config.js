9f3e7d6f8906f8d78f070324046e221a1869664e
aouahib <ouahibdou@gmail.com>
2021-05-16 21:26:19 +0100

    Add robots and generate sitemap

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  webpack: (config, isServer) => {
+    if (isServer) {
+      require("./scripts/generate-sitemap.js");
+    }
+    return config;
+  },
   future: {
     webpack5: true,
   },
---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,39 @@
+const globby = require("globby");
+const {writeFileSync} = require("fs");
+const prettier = require("prettier");
+
+const generateSitemap = async () => {
+  const pages = await globby([
+    "src/pages/**/*.tsx",
+    "!src/pages/**/_*.tsx",
+    "!src/pages/**/[*.tsx",
+    "data/blog"
+  ]);
+  const urls = pages.map(page => {
+    let path = page
+      .replace(".tsx", "")
+      .replace(".md", "");
+    if (path.startsWith("src/pages/")) {
+      path = path.replace("src/pages", "");
+    } else if (path.startsWith("data/blog/")) {
+      path = path.replace("data/blog", "/blog");
+    }
+    if (path.endsWith("/index"))
+      path = path.replace("/index", "");
+    return `
+      <url>
+        <loc>https://abdou.dev${path}</loc>
+      </url>
+    `;
+  });
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls.join("")}    
+    </urlset>
+    `;
+  const formatted = prettier.format(sitemap, {parser: "html"});
+  writeFileSync("public/sitemap.xml", formatted);
+};
+
+void generateSitemap();
