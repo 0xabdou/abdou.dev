d863ddab5d11b7cdb1d11c54aaa5e58e7615029a
aouahib <ouahibdou@gmail.com>
2021-05-17 18:19:09 +0100

    Fix sitemap generator

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,17 +1,56 @@
 const globby = require("globby");
 const {writeFileSync} = require("fs");
 const prettier = require("prettier");
-const tags = require("../data/tags/tags.ts").default;
+
+const tags = {
+  typescript: {
+    name: "typescript",
+    color: "#ffffff",
+    bgColor: "#234A84",
+  },
+  javascript: {
+    name: "javascript",
+    color: "#000000",
+    bgColor: "#F7DF1E",
+  },
+  react: {
+    name: "react",
+    color: "#61DAF6",
+    bgColor: "#222222",
+  },
+  redux: {
+    name: "redux",
+    color: "#ffffff",
+    bgColor: "#764ABC",
+  },
+  nextjs: {
+    name: "nextjs",
+    color: "#ffffff",
+    bgColor: "#000000",
+  },
+  graphql: {
+    name: "graphql",
+    color: "#ff5bc8",
+    bgColor: "#171e26",
+  },
+};
+
+const baseUrl = "https://abdou.dev";
+
+const wrap = (path) => `
+      <url>
+        <loc>${baseUrl}${path}</loc>
+      </url>
+`;
 
 const generateSitemap = async () => {
-  console.log(tags);
   const pages = await globby([
     "src/pages/**/*.tsx",
     "!src/pages/**/_*.tsx",
     "!src/pages/**/[*.tsx",
     "data/blog"
   ]);
-  const urls = pages.map(page => {
+  let urls = pages.map(page => {
     let path = page
       .replace(".tsx", "")
       .replace(".md", "");
@@ -22,12 +61,13 @@ const generateSitemap = async () => {
     }
     if (path.endsWith("/index"))
       path = path.replace("/index", "");
-    return `
-      <url>
-        <loc>https://abdou.dev${path}</loc>
-      </url>
-    `;
+    return wrap(path);
   });
+  urls = [
+    ...urls,
+    wrap("/tags"),
+    ...Object.keys(tags).map(tag => wrap(`/tags/${tag}`))
+  ];
   const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
