d027ad9f2f5dd31823e2a3d9f0899f51343f3a97
aouahib <ouahibdou@gmail.com>
2021-05-18 23:19:02 +0100

    Sort articles by descending date

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -22,20 +22,25 @@ export type Article = {
 
 export const getAllArticles = (): Article[] => {
   const slugs = fs.readdirSync(path.join(root, "data/blog"));
-  return slugs.map(slug => {
-    const raw = fs.readFileSync(path.join(root, "data/blog", slug));
-    const {content, data} = matter(raw);
-    return {
-      content: content,
-      meta: {
-        title: data.title,
-        summary: data.summary,
-        tags: data.tags,
-        publishedAt: data.publishedAt,
-        banner: data.banner,
-        slug: slug.replace(".md", ""),
-        readTime: Math.round(readingTime(content).minutes)
-      }
-    };
-  });
+  return slugs
+    .map(slug => {
+      const raw = fs.readFileSync(path.join(root, "data/blog", slug));
+      const {content, data} = matter(raw);
+      return {
+        content: content,
+        meta: {
+          title: data.title,
+          summary: data.summary,
+          tags: data.tags,
+          publishedAt: data.publishedAt,
+          banner: data.banner,
+          slug: slug.replace(".md", ""),
+          readTime: Math.round(readingTime(content).minutes)
+        }
+      };
+    }).sort((a, b) => {
+      const aDate = new Date(a.meta.publishedAt).getTime();
+      const bDate = new Date(b.meta.publishedAt).getTime();
+      return bDate - aDate;
+    });
 };
