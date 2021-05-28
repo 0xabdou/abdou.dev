dfcfad9c47f32d6a1c4b8e3d639f7380d55ee49e
aouahib <ouahibdou@gmail.com>
2021-05-28 22:44:44 +0100

    Attempt to fix social previews

--- src/components/social-share-preview.tsx
+++ src/components/social-share-preview.tsx
@@ -11,13 +11,18 @@ const SocialSharePreview = (props: TwitterCardProps) => {
     <Head>
       <meta name="twitter:card" content="summary_large_image"/>
       <meta name="twitter:creator" content="@aouahib_"/>
+      <meta property="og:title" content={props.title}/>
+      <meta property="og:description" content={props.description}/>
+      <meta
+        property="og:image"
+        content={`https://abdou.dev${props.image}`}
+      />
+      // For stupid linkedin
       <meta
         name="image"
         property="og:image"
         content={`https://abdou.dev${props.image}`}
       />
-      <meta property="og:title" content={props.title}/>
-      <meta property="og:description" content={props.description}/>
     </Head>
   );
 };
