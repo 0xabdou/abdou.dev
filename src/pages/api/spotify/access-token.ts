51e2a7a37770e34cbba8d67e26577dbca4510522
aouahib <ouahibdou@gmail.com>
2021-06-02 23:42:39 +0100

    Spotify API Checkpoint 3

--- src/pages/api/spotify/access-token.ts
+++ src/pages/api/spotify/access-token.ts
@@ -9,19 +9,20 @@ const handler: NextApiHandler = async (req, res) => {
     }
     const url = "https://accounts.spotify.com/api/token";
     const auth = `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`;
+    console.log("AUTH: ", btoa(auth));
     const response = await fetch(
       url,
       {
         method: "POST",
-        headers: {
-          "Authorization": `Basic ${btoa(auth)}`
-        },
         body: new URLSearchParams({
           grant_type: "refresh_token",
-          refresh_token: `${process.env.SPOTIFY_REFRESH_TOKEN}`
+          refresh_token: `${process.env.SPOTIFY_REFRESH_TOKEN}`,
+          client_id: `4252246e16d94478a423d340eda0cba0`,
+          client_secret: `c82cd54afa90481889d4056fa1781361`
         })
       }
     );
+    console.log("RESP: ", await response.text());
     const {access_token} = await response.json();
     console.log("GOT ACCESS TOKEN: ", access_token);
     return res.status(200).json({access_token});
