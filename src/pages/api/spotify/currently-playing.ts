02868c0d71fa06a8d85879aa84f4a23f0cecb5a3
aouahib <ouahibdou@gmail.com>
2021-06-04 18:29:41 +0100

    Spotify checkpoint

--- src/pages/api/spotify/currently-playing.ts
+++ src/pages/api/spotify/currently-playing.ts
@@ -8,6 +8,15 @@ const handler: NextApiHandler = async (_, res) => {
     url,
     {headers: {"Authorization": `Bearer ${access_token}`}}
   );
+  if (response.status != 200) {
+    const error = {
+      status: response.status,
+      statusText: response.statusText,
+      error: await response.text(),
+    };
+    console.log("ERROR: ", error);
+    return res.status(response.status).json(error);
+  }
   const json = await response.json();
   res.status(200).json(json);
 };
